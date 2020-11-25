a7ea92aae7591cf31c6cd110e667697049bae396
ninedraft <clamores.pro@gmail.com>
2020-11-25 22:36:41 +0300

    fix(server): :bug: use localhost

--- server.py
+++ server.py
@@ -20,6 +20,7 @@ server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 server.settimeout(0.2)
 message = b"your very important message"
 while True:
+    server.sendto(message, ("localhost", 37020))
     print("message sent!", flush=True)
     time.sleep(1)
 
