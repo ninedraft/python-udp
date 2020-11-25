4da105cc1bb307ab0840f9d8cc35adf24c8308e5
ninedraft <clamores.pro@gmail.com>
2020-11-25 22:32:50 +0300

    fix(server): :bug: enable flushing for log messages

--- server.py
+++ server.py
@@ -20,7 +20,6 @@ server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 server.settimeout(0.2)
 message = b"your very important message"
 while True:
-    server.sendto(message, ("<broadcast>", 37020))
-    print("message sent!")
+    print("message sent!", flush=True)
     time.sleep(1)
 
